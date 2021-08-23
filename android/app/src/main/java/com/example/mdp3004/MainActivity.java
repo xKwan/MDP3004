27a20bd64753fd02ba1bb06371d87c1b1f2737f4
xKwan <48723840+xKwan@users.noreply.github.com>
2021-08-23 22:27:21 +0800

    Implement bluetooth

---
+++ android/app/src/main/java/com/example/mdp3004/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.mdp3004;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+import com.example.mdp3_android.bluetooth.BluetoothActivity;
+import com.example.mdp3_android.bluetooth.BluetoothService;
+
+
+
+public class MainActivity extends FlutterActivity {
+}
